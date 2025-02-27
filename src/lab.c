948eed9b6fdfdafcbfad75b9234b56973ddf2a12
Carson <123137691+cjk-bsu@users.noreply.github.com>
2025-02-27 02:40:41 +0000

    Completed shell functionality

--- src/lab.c
+++ src/lab.c
@@ -15,66 +15,60 @@
 #include <termios.h>
 
 char *get_prompt(const char *env) {
-    char *prompt = getenv(env);
-    if (!prompt) {
-        prompt = "shell>";
+    const char *temp_prompt = getenv(env);
+    if (!temp_prompt) {
+        temp_prompt = "shell>";
     }
-    return strdup(prompt);
 
-    // char *prompt = getenv(env);
-    // if (!prompt) {
-    //     return strdup("shell>");
-    // } 
-    // return strdup(prompt);
+    char *prompt = malloc(strlen(temp_prompt) + 1);
+    if (!prompt) {
+        perror("get_prompt: malloc failure");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(prompt, temp_prompt);
+    return prompt;
 }
 
 char **cmd_parse(const char *line) {
-    if (!line) return NULL;
-
-    long arg_max = sysconf(_SC_ARG_MAX);
-    if (arg_max <= 0) arg_max = 4096;
-
-    char **args = malloc((arg_max / 4) * sizeof(char *));
-    if (!args) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-
-    char *token;
+    const long arg_max = sysconf(_SC_ARG_MAX);
     char *line_copy = strdup(line);
+    char *save = NULL;
+
     if (!line_copy) {
-        perror("strdup");
-        cmd_free(args);
+        perror("cmd_parse: strdup failed");
         exit(EXIT_FAILURE);
     }
 
-    int i = 0;
-    token = strtok(line_copy, " ");
-    while (token != NULL && i < (arg_max / 2) - 1) {
-        if (token) {
-            args[i] = strdup(token);
-            if (!args[i]) {
-                perror("strdup");
-                cmd_free(args);
-                free(line_copy);
-                exit(EXIT_FAILURE);
-            }
-        }
-        token = strtok(NULL, " ");
-        i++;
+    char **args = (char **)calloc(arg_max, sizeof(char *));
+
+    if (!args) {
+        perror("cmd_parse: calloc failed");
+        free(line_copy);
+        exit(EXIT_FAILURE);
     }
-    args[i] = NULL;
-    free(line_copy);
+
+    char *token = strtok_r(line_copy, " ", &save);
+
+    if (!token) {
+        perror("cmd_parse: strtok failed");
+        cmd_free(args);
+        free(line_copy);
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; token && i < arg_max; i++) {
+        args[i] = token;
+        token = strtok_r(NULL, " ", &save);
+    }
+
     return args;
 }
 
 void cmd_free(char **args) {
-    if (!args) return;
-
-    for (int i = 0; args[i] != NULL; i++) {
-        free(args[i]);
-    }
-    free(args);
+    char *temp_args = *args;
+    free(temp_args);
+    free((void *)args);
 }
 
 char *trim_white(char *line) {
@@ -100,9 +94,11 @@ void parse_args(int argc, char **argv) {
         switch (opt) {
             case 'v':
                 printf("Shell Version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+                cmd_free(argv);
                 exit(EXIT_SUCCESS);
             default:
                 fprintf(stderr, "Usage: %s [-v]\n", argv[0]);
+                cmd_free(argv);
                 exit(EXIT_FAILURE);
         }
     }
@@ -120,118 +116,95 @@ int change_dir(char **dir) {
     return chdir(dir[1]);
 }
 
-// void add_history(const char *line) {
-//     if (line && *line) {
-//         add_history(line); 
-//     }
-// }
-
-// void print_history() {
-//     HIST_ENTRY **history_entries = history_list();  
-//     if (history_entries) {
-//         for (int i = 0; history_entries[i] != NULL; i++) {
-//             printf("%d: %s\n", i + 1, history_entries[i]->line);
-//         }
-//     } else {
-//         printf("No history available.\n");
-//     }
-// }
-
 bool do_builtin(struct shell *sh, char **argv) {
     if (!argv || !argv[0]) {
-        return false;
+        cmd_free(argv);
+        return true;
     }
 
     if (strcmp(argv[0], "exit") == 0) {
         cmd_free(argv);
-        sh_destroy(sh); 
+        sh_destroy(sh);
         exit(EXIT_FAILURE);
     }
 
     if (strcmp(argv[0], "cd") == 0) {
         if (change_dir(argv) != 0) {
-            perror("cd");
-            cmd_free(argv);
-            sh_destroy(sh);
-            exit(EXIT_FAILURE);
+            return false;
         }
-        cmd_free(argv); 
+
         return true;
     }
 
-    // if (strcmp(argv[0], "history") == 0) {
-    //     print_history(); 
-    //     cmd_free(argv);
-    //     return true;
-    // }
+    if (strcmp(argv[0], "history") == 0) {
+        HIST_ENTRY **history = history_list();
+        if(history == NULL) {
+            return false;
+        }
+        //print history
+        for(int i = 0; history[i] != NULL; i++) {
+            printf("%d %s\n", i, history[i]->line);
+        }
+
+        return true;
+    }
 
     pid_t pid = fork();
     if (pid == -1) {
-        perror("fork");
+        perror("do_builtin: fork failure");
         return false; 
     } else if (pid == 0) {
         execvp(argv[0], argv); 
-        perror("execvp");
-        exit(EXIT_FAILURE); 
+        perror("do_builtin: execvp failure"); 
+        return false;
     } else {
         int status;
         waitpid(pid, &status, 0); 
+        if (status < 0) {
+            perror("do_builtin: waitpid failure");
+            return false;
+        }
     }
 
-    cmd_free(argv);
     return true;
 }
 
 void sh_init(struct shell *sh) {
+
+    using_history();
+
+    signal(SIGINT, SIG_IGN);
+    signal(SIGQUIT, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+    signal(SIGTTIN, SIG_IGN);
+    signal(SIGTTOU, SIG_IGN);
+    signal(SIGCHLD, SIG_IGN);
+
+    sh->prompt = get_prompt("SHELL_PROMPT");
+
     sh->shell_terminal = STDIN_FILENO;
     sh->shell_is_interactive = isatty(sh->shell_terminal);
+
+    sh->shell_pgid = getpgrp();
 
     if (!sh->shell_is_interactive) {
         while (tcgetpgrp(sh->shell_terminal) != (sh->shell_pgid = getpgrp())) kill (- sh->shell_pgid, SIGTTIN);
 
-        // ignore signals
-        signal(SIGINT, SIG_IGN);
-        signal(SIGQUIT, SIG_IGN);
-        signal(SIGTSTP, SIG_IGN);
-        signal(SIGTTIN, SIG_IGN);
-        signal(SIGTTOU, SIG_IGN);
-        signal(SIGCHLD, SIG_IGN);
-
         sh->shell_pgid = getpid();
         if (setpgid(sh->shell_pgid, sh->shell_pgid) < 0) {
-            perror("sh_init: setpgid failed");
+            perror("sh_init: setpgid failure");
             sh_destroy(sh);
             exit(EXIT_FAILURE);
         }
 
         tcsetpgrp(sh->shell_terminal, sh->shell_pgid);
-        if (tcsetpgrp(sh->shell_terminal, sh->shell_pgid) < 0) {
-            perror("sh_init: tcsetpgrp failed");
-            sh_destroy(sh);
-            exit(EXIT_FAILURE);
-        }
 
         tcgetattr(sh->shell_terminal, &sh->shell_tmodes);
-        if (tcgetattr(sh->shell_terminal, &sh->shell_tmodes) < 0) {
-            perror("sh_init: tcgetattr failed");
-            sh_destroy(sh);
-            exit(EXIT_FAILURE);
-        }
     }
-
-    sh->prompt = get_prompt("SHELL_PROMPT");
 }
-
 
 void sh_destroy(struct shell *sh) {
-    if (sh != NULL) {
-        if (sh->prompt != NULL) {
-            free(sh->prompt);
-        }
-        free(sh);
-    }
+    free(sh->prompt);
+    free(sh);
+    clear_history();
 }
-
-// // void sh_destroy(struct shell *sh) {
-// //     free(sh->prompt);
-// // }
